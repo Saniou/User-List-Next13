5f1f8f23e4f4f7b40d27a021c6543165df6eb6d3
Olexander Krasulia <krasulazr@gmail.com>
2023-05-15 14:00:56 +0300

    Search bar Completed

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -18,19 +18,41 @@ export const SearchUser = () => {
   const [user, setUser] = useState<Notice | null>(null);
   const [search, setSearch] = useState<string>('');
   const [loading, setLoading] = useState(false);
+  const [suggestedUsers, setSuggestedUsers] = useState<Notice[]>([]);
 
   async function fetchData() {
     setLoading(true);
-    const data = await fetch(`https://dummyjson.com/users?limit=100&search=${search}`, {
+    const data = await fetch(`https://dummyjson.com/users?limit=100`, {
       method: 'GET',
     }).then((res) => res.json());
-     setUser(data.users.find((u: Notice) => u.firstName  === search) || null);
+  
+    const [firstName, lastName] = search.split(' ');
+  
+    const filteredUsers = data.users.filter((u: Notice) => {
+      const firstNameMatch = firstName && u.firstName.toLowerCase() === firstName.toLowerCase();
+      const lastNameMatch = lastName && u.lastName.toLowerCase() === lastName.toLowerCase();
+      return firstNameMatch || lastNameMatch;
+    });
+  
+    setUser(filteredUsers.length > 0 ? filteredUsers[0] : null);
     setLoading(false);
+  }
+  
+
+  async function fetchSuggestedUsers() {
+    const data = await fetch(`https://dummyjson.com/users?limit=100`, {
+      method: 'GET',
+    }).then((res) => res.json());
+    setSuggestedUsers(data.users);
   }
 
   useEffect(() => {
     fetchData();
   }, [search]);
+
+  useEffect(() => {
+    fetchSuggestedUsers();
+  }, []);
 
   return (
     <div className="w-[50%] relative text-gray-600 mx-auto mb-8 my-2">
@@ -40,7 +62,13 @@ export const SearchUser = () => {
         name="search"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
+        list="suggestedUsers"
       />
+      <datalist id="suggestedUsers">
+        {suggestedUsers.map((user) => (
+          <option key={user.id} value={`${user.firstName} ${user.lastName}`} />
+        ))}
+      </datalist>
       <button
         type="submit"
         className="cursor-pointer absolute top-0 right-0 mt-3 mr-2"
@@ -51,19 +79,19 @@ export const SearchUser = () => {
       {loading ? (
         <p>Loading...</p>
       ) : user ? (
-        <div >
-        <Users
-          key={user.id}
-          id={user.id}
-          firstName={user.firstName}
-          lastName={user.lastName}
-          image={user.image}
-          age={user.age}
-          gender={user.gender}
-          email={user.email}
-          phone={user.phone}
-          username={user.username}
-        />
+        <div className='justify-center items-center mt-8 w-[40%] ml-[30%]'>
+          <Users
+            key={user.id}
+            id={user.id}
+            firstName={user.firstName}
+            lastName={user.lastName}
+            image={user.image}
+            age={user.age}
+            gender={user.gender}
+            email={user.email}
+            phone={user.phone}
+            username={user.username}
+          />
         </div>
       ) : (
         <p>No results found for "{search}".</p>
